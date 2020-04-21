a4f5d9eeb46a47ecd438b1d863a9328de8c5b988
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-04-21 12:35:27 +0200

    vue axious setup

--- public/js/app.js
+++ public/js/app.js
@@ -1899,6 +1899,107 @@ module.exports = {
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Login.vue?vue&type=script&lang=js&":
+/*!****************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Login.vue?vue&type=script&lang=js& ***!
+  \****************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      errors: [],
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    close: function close() {
+      this.$emit("close");
+    },
+    checkForm: function checkForm(e) {
+      var _this = this;
+
+      this.errors = [];
+
+      if (this.email && this.password) {
+        axios__WEBPACK_IMPORTED_MODULE_0___default.a.post(window.location.origin + "/api/login", {
+          "email": this.email,
+          "password": this.password
+        }).then(function (response) {})["catch"](function (e) {
+          _this.errors.push(e);
+        });
+      }
+
+      if (!this.email) {
+        this.errors.push('email required.');
+      }
+
+      if (!this.password) {
+        this.errors.push('password required.');
+      }
+
+      e.preventDefault();
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/views/App.vue?vue&type=script&lang=js&":
 /*!*********************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/views/App.vue?vue&type=script&lang=js& ***!
@@ -1908,6 +2009,7 @@ module.exports = {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_Login_vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../components/Login.vue */ "./resources/js/components/Login.vue");
 //
 //
 //
@@ -1927,11 +2029,23 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
-      hamburger: true
+      hamburger: true,
+      modal_login_open: false
     };
+  },
+  methods: {
+    closeLogin: function closeLogin() {
+      this.modal_login_open = false;
+    }
+  },
+  components: {
+    login: _components_Login_vue__WEBPACK_IMPORTED_MODULE_0__["default"]
   }
 });
 
@@ -37643,14 +37757,99 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c("div", [
+    _c("br"),
+    _c("br"),
+    _c("br"),
+    _c("br"),
+    _c("br"),
+    _c("br"),
+    _vm._v(" "),
+    _c("button", { on: { click: _vm.close } }, [
+      _vm._v("Close modal bla bla bla")
+    ]),
+    _vm._v(" "),
+    _c(
+      "form",
+      { attrs: { id: "app", method: "post" }, on: { submit: _vm.checkForm } },
+      [
+        _vm.errors.length
+          ? _c("div", [
+              _c("b", [_vm._v("Please correct the following error(s):")]),
+              _vm._v(" "),
+              _c(
+                "ul",
+                _vm._l(_vm.errors, function(error) {
+                  return _c("li", [_vm._v(_vm._s(error))])
+                }),
+                0
+              )
+            ])
+          : _vm._e(),
+        _vm._v(" "),
+        _c("p", [
+          _c("label", { attrs: { for: "email" } }, [_vm._v("email")]),
+          _vm._v(" "),
+          _c("input", {
+            directives: [
+              {
+                name: "model",
+                rawName: "v-model",
+                value: _vm.email,
+                expression: "email"
+              }
+            ],
+            attrs: { id: "email", type: "email", name: "email" },
+            domProps: { value: _vm.email },
+            on: {
+              input: function($event) {
+                if ($event.target.composing) {
+                  return
+                }
+                _vm.email = $event.target.value
+              }
+            }
+          })
+        ]),
+        _vm._v(" "),
+        _c("p", [
+          _c("label", { attrs: { for: "password" } }, [_vm._v("password")]),
+          _vm._v(" "),
+          _c("input", {
+            directives: [
+              {
+                name: "model",
+                rawName: "v-model",
+                value: _vm.password,
+                expression: "password"
+              }
+            ],
+            attrs: { id: "password", type: "password", name: "password" },
+            domProps: { value: _vm.password },
+            on: {
+              input: function($event) {
+                if ($event.target.composing) {
+                  return
+                }
+                _vm.password = $event.target.value
+              }
+            }
+          })
+        ]),
+        _vm._v(" "),
+        _vm._m(0)
+      ]
+    )
+  ])
 }
 var staticRenderFns = [
   function() {
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", [_c("h1", [_vm._v("Login component")])])
+    return _c("p", [
+      _c("input", { attrs: { type: "submit", value: "Submit" } })
+    ])
   }
 ]
 render._withStripped = true
@@ -37674,55 +37873,77 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [
-    _c("div", { staticClass: "navbar" }, [
-      _c("div", { staticClass: "navbar-inner" }, [
-        _c("img", {
-          staticClass: "navbar-logo",
-          attrs: { src: "/img/nz-logo-light.png" }
-        }),
-        _vm._v(" "),
-        _c(
-          "button",
-          {
-            staticClass: "navbar-hamburger",
-            on: {
-              click: function($event) {
-                _vm.hamburger = false
+  return _c(
+    "div",
+    [
+      _c("div", { staticClass: "navbar" }, [
+        _c("div", { staticClass: "navbar-inner" }, [
+          _c("img", {
+            staticClass: "navbar-logo",
+            attrs: { src: "/img/nz-logo-light.png" }
+          }),
+          _vm._v(" "),
+          _c(
+            "button",
+            {
+              staticClass: "navbar-hamburger",
+              on: {
+                click: function($event) {
+                  _vm.hamburger = false
+                }
               }
-            }
-          },
-          [_vm._v(_vm._s(_vm.hamburger))]
-        )
-      ])
-    ]),
-    _vm._v(" "),
-    _c(
-      "div",
-      { staticClass: "sidenav", class: { "sidenav-hidden": _vm.hamburger } },
-      [
-        _c(
-          "button",
-          {
-            staticClass: "sidenav-close",
-            on: {
-              click: function($event) {
-                _vm.hamburger = true
+            },
+            [_vm._v(_vm._s(_vm.hamburger))]
+          )
+        ])
+      ]),
+      _vm._v(" "),
+      _c(
+        "div",
+        { staticClass: "sidenav", class: { "sidenav-hidden": _vm.hamburger } },
+        [
+          _c(
+            "button",
+            {
+              staticClass: "sidenav-close",
+              on: {
+                click: function($event) {
+                  _vm.hamburger = true
+                }
               }
-            }
-          },
-          [_vm._v("close")]
-        ),
-        _vm._v(" "),
-        _c("router-link", { attrs: { to: "/", exact: "" } }, [_vm._v("home")]),
-        _vm._v(" "),
-        _c("router-link", { attrs: { to: "/about" } }, [_vm._v("about")])
-      ],
-      1
-    ),
-    _vm._v(" "),
-    _c("div", { staticClass: "main" }, [_c("router-view")], 1)
-  ])
+            },
+            [_vm._v("close")]
+          ),
+          _vm._v(" "),
+          _c("router-link", { attrs: { to: "/", exact: "" } }, [
+            _vm._v("home")
+          ]),
+          _vm._v(" "),
+          _c("router-link", { attrs: { to: "/about" } }, [_vm._v("about")]),
+          _vm._v(" "),
+          _c(
+            "button",
+            {
+              on: {
+                click: function($event) {
+                  _vm.modal_login_open = true
+                }
+              }
+            },
+            [_vm._v("Login")]
+          )
+        ],
+        1
+      ),
+      _vm._v(" "),
+      _vm.modal_login_open
+        ? _c("login", { on: { close: _vm.closeLogin } })
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "main" }, [_c("router-view")], 1)
+    ],
+    1
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -53010,15 +53231,17 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Login_vue_vue_type_template_id_6bdc8b8e___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Login.vue?vue&type=template&id=6bdc8b8e& */ "./resources/js/components/Login.vue?vue&type=template&id=6bdc8b8e&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _Login_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Login.vue?vue&type=script&lang=js& */ "./resources/js/components/Login.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Login_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _Login_vue_vue_type_template_id_6bdc8b8e___WEBPACK_IMPORTED_MODULE_0__["render"],
   _Login_vue_vue_type_template_id_6bdc8b8e___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -53032,6 +53255,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/components/Login.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/Login.vue?vue&type=script&lang=js&":
+/*!********************************************************************!*\
+  !*** ./resources/js/components/Login.vue?vue&type=script&lang=js& ***!
+  \********************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Login_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./Login.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Login.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Login_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="navbar"><!--Nav Bar-->
+        <div class="navbar">
             <div class="navbar-inner">
                 <img class="navbar-logo" src="/img/nz-logo-light.png">
                 <button class="navbar-hamburger" @click="hamburger = false">{{hamburger}}</button>
@@ -10,19 +10,30 @@
             <button class="sidenav-close" @click="hamburger = true">close</button>
             <router-link to="/" exact>home</router-link>
             <router-link to="/about">about</router-link>
+            <button @click="modal_login_open = true">Login</button>
         </div>
+        <login v-if="modal_login_open" @close="closeLogin"></login>
         <div class="main">
-        <router-view></router-view>
+            <router-view></router-view>
         </div>
     </div>
 </template>
 
 <script>
+    import login from '../components/Login.vue'
+
     export default {
         data() {
             return {
                 hamburger: true,
+                modal_login_open: false,
             }
-        }
+        },
+        methods:{
+          closeLogin(){
+              this.modal_login_open = false;
+          }
+        },
+        components: {login},
     }
 </script>
