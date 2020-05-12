841cf91708210607a87f3f37182384393dafa24d
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-05-12 16:26:44 +0200

    updated some vue things for the homepage and checked some styling

--- public/js/app.js
+++ public/js/app.js
@@ -1948,6 +1948,74 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Home.vue?vue&type=script&lang=js&":
+/*!***************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Home.vue?vue&type=script&lang=js& ***!
+  \***************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      items: [{
+        title: 'TIDES Europe',
+        description: 'Tides offers the science,\n' + 'technologies and partners you\n' + 'need to grow your business',
+        date: new Date(),
+        location: 'Amsterdam'
+      }, {
+        title: 'TIDES Europe',
+        description: 'Tides offers the science,\n' + 'technologies and partners you\n' + 'need to grow your business',
+        date: new Date(),
+        location: 'Amsterdam'
+      }, {
+        title: 'TIDES Europe',
+        description: 'Tides offers the science,\n' + 'technologies and partners you\n' + 'need to grow your business',
+        date: new Date(),
+        location: 'Amsterdam'
+      }]
+    };
+  },
+  methods: {
+    formatDate: function formatDate(date) {
+      var datetime = new Date(date);
+      var formatted_date = datetime.getDate() + "-" + (datetime.getMonth() + 1) + "-" + datetime.getFullYear();
+      return formatted_date;
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Login.vue?vue&type=script&lang=js&":
 /*!****************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Login.vue?vue&type=script&lang=js& ***!
@@ -38041,74 +38109,57 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
-}
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "homepage-full" }, [
-      _c("h1", [_vm._v("Evenementen")]),
-      _vm._v(" "),
-      _c("div", { staticClass: "flex-wrapper" }, [
-        _c("div", { staticClass: "home-events flex-grid" }, [
-          _c("div", { staticClass: "home-event column-desktop-4" }, [
-            _c("h2", [_vm._v("Event Title")]),
-            _vm._v(" "),
-            _c("hr", { staticClass: "home-divider" }),
-            _vm._v(" "),
-            _c("p", { staticClass: "home-description" }, [
-              _vm._v(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare pulvinar augue, a ullamcorper lectus."
-              )
-            ]),
-            _vm._v(" "),
-            _c("hr", { staticClass: "home-divider-2" }),
-            _vm._v(" "),
-            _c("div", { staticClass: "flex-wrapper" }, [
-              _c("div", { staticClass: "flex-grid" }, [
-                _c("p", { staticClass: "home-date column-desktop-6" }, [
-                  _vm._v("Datum")
-                ]),
-                _vm._v(" "),
-                _c("p", { staticClass: "home-location column-desktop-6" }, [
-                  _vm._v("Locatie")
+  return _c("div", { staticClass: "homepage-full" }, [
+    _c("h1", [_vm._v("Evenementen")]),
+    _vm._v(" "),
+    _c("div", { staticClass: "flex-wrapper" }, [
+      _c(
+        "div",
+        { staticClass: "home-events flex-grid" },
+        _vm._l(_vm.items, function(item) {
+          return _c(
+            "div",
+            {
+              staticClass:
+                "home-event column-desktop-4 column-tablet-12 column-mobile-12"
+            },
+            [
+              _c("div", { staticClass: "home-event-background" }),
+              _vm._v(" "),
+              _c("h2", [_vm._v(_vm._s(item.title))]),
+              _vm._v(" "),
+              _c("hr", { staticClass: "home-divider" }),
+              _vm._v(" "),
+              _c("p", { staticClass: "home-description" }, [
+                _vm._v(
+                  "\n                    " +
+                    _vm._s(item.description) +
+                    "\n                "
+                )
+              ]),
+              _vm._v(" "),
+              _c("hr", { staticClass: "home-divider-2" }),
+              _vm._v(" "),
+              _c("div", { staticClass: "flex-wrapper" }, [
+                _c("div", { staticClass: "flex-grid" }, [
+                  _c("p", { staticClass: "home-date column-desktop-6" }, [
+                    _vm._v(_vm._s(_vm.formatDate(item.date)))
+                  ]),
+                  _vm._v(" "),
+                  _c("p", { staticClass: "home-location column-desktop-6" }, [
+                    _vm._v(_vm._s(item.location))
+                  ])
                 ])
               ])
-            ])
-          ]),
-          _vm._v(" "),
-          _c("div", { staticClass: "home-event column-desktop-4" }, [
-            _c("h2", [_vm._v("Event Title 2")]),
-            _vm._v(" "),
-            _c("hr", { staticClass: "home-divider" }),
-            _vm._v(" "),
-            _c("p", { staticClass: "home-description" }, [
-              _vm._v(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare pulvinar augue, a ullamcorper lectus."
-              )
-            ]),
-            _vm._v(" "),
-            _c("hr", { staticClass: "home-divider-2" }),
-            _vm._v(" "),
-            _c("div", { staticClass: "flex-wrapper" }, [
-              _c("div", { staticClass: "flex-grid" }, [
-                _c("p", { staticClass: "home-date column-desktop-6" }, [
-                  _vm._v("Datum")
-                ]),
-                _vm._v(" "),
-                _c("p", { staticClass: "home-location column-desktop-6" }, [
-                  _vm._v("Locatie")
-                ])
-              ])
-            ])
-          ])
-        ])
-      ])
+            ]
+          )
+        }),
+        0
+      )
     ])
-  }
-]
+  ])
+}
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -53906,15 +53957,17 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Home_vue_vue_type_template_id_f2b6376c___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Home.vue?vue&type=template&id=f2b6376c& */ "./resources/js/components/Home.vue?vue&type=template&id=f2b6376c&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _Home_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Home.vue?vue&type=script&lang=js& */ "./resources/js/components/Home.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Home_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _Home_vue_vue_type_template_id_f2b6376c___WEBPACK_IMPORTED_MODULE_0__["render"],
   _Home_vue_vue_type_template_id_f2b6376c___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -53928,6 +53981,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/components/Home.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/Home.vue?vue&type=script&lang=js&":
+/*!*******************************************************************!*\
+  !*** ./resources/js/components/Home.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Home_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./Home.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Home.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Home_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
@@ -54356,8 +54423,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\lemme\PhpstormProjects\nz-evenementenregistratie\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\lemme\PhpstormProjects\nz-evenementenregistratie\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Tim Huijkman\PhpstormProjects\nz-evenementenregistratie\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Tim Huijkman\PhpstormProjects\nz-evenementenregistratie\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -4,28 +4,18 @@
 
         <div class="flex-wrapper">
             <div class="home-events flex-grid">
-                <div class="home-event column-desktop-4">
-                   <h2>Event Title</h2>
+                <div v-for="item in items" class="home-event column-desktop-4 column-tablet-12 column-mobile-12">
+                    <div class="home-event-background"></div>
+                    <h2>{{item.title}}</h2>
                     <hr class="home-divider">
-                    <p class="home-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare pulvinar augue, a ullamcorper lectus.</p>
+                    <p class="home-description">
+                        {{item.description}}
+                    </p>
                     <hr class="home-divider-2">
                     <div class="flex-wrapper">
                         <div class="flex-grid">
-                            <p class="home-date column-desktop-6">Datum</p>
-                            <p class="home-location column-desktop-6">Locatie</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="home-event column-desktop-4">
-                    <h2>Event Title 2</h2>
-                    <hr class="home-divider">
-                    <p class="home-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare pulvinar augue, a ullamcorper lectus.</p>
-                    <hr class="home-divider-2">
-                    <div class="flex-wrapper">
-                        <div class="flex-grid">
-                            <p class="home-date column-desktop-6">Datum</p>
-                            <p class="home-location column-desktop-6">Locatie</p>
+                            <p class="home-date column-desktop-6">{{formatDate(item.date)}}</p>
+                            <p class="home-location column-desktop-6">{{item.location}}</p>
                         </div>
                     </div>
                 </div>
@@ -34,3 +24,47 @@
 
     </div>
 </template>
+
+<script>
+    export default ({
+        data(){
+            return{
+                items: [
+                    {
+                        title: 'TIDES Europe',
+                        description:
+                            'Tides offers the science,\n' +
+                            'technologies and partners you\n' +
+                            'need to grow your business',
+                        date: new Date(),
+                        location: 'Amsterdam'
+                    },
+                    {
+                        title: 'TIDES Europe',
+                        description:
+                            'Tides offers the science,\n' +
+                            'technologies and partners you\n' +
+                            'need to grow your business',
+                        date: new Date(),
+                        location: 'Amsterdam'
+                    },
+                    {
+                        title: 'TIDES Europe',
+                        description:
+                            'Tides offers the science,\n' +
+                            'technologies and partners you\n' +
+                            'need to grow your business',
+                        date: new Date(),
+                        location: 'Amsterdam'
+                    }],
+            }
+        },
+        methods:{
+            formatDate(date){
+                let datetime = new Date(date);
+                let formatted_date = datetime.getDate() + "-" + (datetime.getMonth() + 1) + "-" + datetime.getFullYear()
+                return formatted_date
+            }
+        }
+    });
+</script>
