7b10e2f1648894a9e35a4cf4252e82171b4e942e
Merlijn Busch <24628@ma-web.nl>
2020-06-01 14:41:27 +0200

    small update

--- resources/js/views/Profile.vue
+++ resources/js/views/Profile.vue
@@ -8,7 +8,7 @@
                 <div class="column-desktop-4 column-tablet-6 column-mobile-12 profile-border profile-main-wrapper">
                     <p class="profile-edit" :class="{'profile-save-padding': edit}"><a href="#" @click="edit = !edit"
                                                                                        id="disableLink"><span>Profiel aanpassen </span><i
-                            class="fas fa-cog"></i></a></p>
+                        class="fas fa-cog"></i></a></p>
                     <input type="submit" :class="{'profile-save': edit, 'invis': !edit}" v-if=" edit"
                            value="Profiel opslaan">
                     <div class="profile-foto" :style="!!image ? {backgroundImage: 'url('+image+')'} : ''">
@@ -36,7 +36,8 @@
                         <p class="contact-title">Contact</p>
                         <p class="contact-logo">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 8c0 .557-.447 1.008-1 1.008s-1-.45-1-1.008c0-.557.447-1.008 1-1.008s1 .452 1 1.008zm0 2h-2v6h2v-6zm3 0h-2v6h2v-2.861c0-1.722 2.002-1.881 2.002 0v2.861h1.998v-3.359c0-3.284-3.128-3.164-4-1.548v-1.093z"/>
+                                <path
+                                    d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 8c0 .557-.447 1.008-1 1.008s-1-.45-1-1.008c0-.557.447-1.008 1-1.008s1 .452 1 1.008zm0 2h-2v6h2v-6zm3 0h-2v6h2v-2.861c0-1.722 2.002-1.881 2.002 0v2.861h1.998v-3.359c0-3.284-3.128-3.164-4-1.548v-1.093z"/>
                             </svg>
                         </p>
                     </div>
@@ -68,10 +69,8 @@
                 edit: false,
                 image: null,
             }
-            document.getElementById("disableLink").addEventListener("click", function (event) {
-                event.preventDefault()
-            });
-        }, async mounted() {
+        },
+        async mounted() {
             const res = await API.get("/api/profile-check");
 
             if (!res.data) return
@@ -131,10 +130,6 @@
                 await API.post(data, '/api/profile-edit');
                 await API.post(this.role_name, '/api/role/' + this.user_id)
             }
-
         }
-
     }
-
-
 </script>
