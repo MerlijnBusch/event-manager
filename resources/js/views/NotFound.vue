d1dd387e87e1f2d4ad8cf0db2e2366f14841a39c
ticho10 <poffertjeskip@gmail.com>
2020-06-02 08:53:45 +0200

    404 image

--- resources/js/views/NotFound.vue
+++ resources/js/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wrapper-404">
-      <img src="/img/NHN-business-awards.jpg" alt="">
+      <img src="/img/header1.jpg" alt="">
       <h2>Ooh, je bent perongelijk naar een niet bestaande pagina gestuurd.</h2>
       <h3><a href="/">Klik hier</a> om terug te gaan naar de homepage.</h3>
     </div>
