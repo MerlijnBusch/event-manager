6522b749c50f6e8b4346bb165603cca9d18749d3
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-06-03 10:55:27 +0200

    added smart search delay of 500ms

--- resources/js/views/Search.vue
+++ resources/js/views/Search.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="search">
-
+        <label>
+            Search:
+            <input type="text" v-model="searchString">
+            <p>{{searchString}}</p>
+        </label>
     </div>
 </template>
 
@@ -9,7 +13,27 @@
         name: 'search',
         data(){
             return{
+                searchString: '',
+                oldSearchString: '',
+                sendTimeout: null,
+            }
+        },
+        watch:{
+          searchString(){
+              this.send()
+            }
+        },
+        methods: {
+            send(){
+                if (this.searchString.length && this.searchString !== this.oldSearchString){
+                    this.oldSearchString = this.searchString;
+                    clearTimeout(this.sendTimeout);
+                    this.sendTimeout = setTimeout(()=>{
+                        //TODO: call here
+                        console.log(this.searchString);
+                    },500);
 
+                }
             }
         }
     }
