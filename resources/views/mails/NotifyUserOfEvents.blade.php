10c279f9117efe6248933b6da265c5c323b7dc2c
Merlijn Busch <24628@ma-web.nl>
2020-06-15 14:57:15 +0200

    formatting shit

--- resources/views/mails/NotifyUserOfEvents.blade.php
+++ resources/views/mails/NotifyUserOfEvents.blade.php
@@ -1,87 +1,139 @@
 <!DOCTYPE html >
-<html >
+<html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
-<body style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -webkit-text-size-adjust: none; background-color: #404040; color: #C1C1C1; height: 100%; line-height: 1.4; margin: 0; padding: 0; width: 100% !important;">
+<body
+    style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -webkit-text-size-adjust: none; background-color: #404040; color: #C1C1C1; height: 100%; line-height: 1.4; margin: 0; padding: 0; width: 100% !important;">
 <style>
-@media  only screen and (max-width: 600px) {
-.inner-body {
-width: 100% !important;
-}
+    @media only screen and (max-width: 600px) {
+        .inner-body {
+            width: 100% !important;
+        }
 
-.footer {
-width: 100% !important;
-}
-}
+        .footer {
+            width: 100% !important;
+        }
+    }
 
-@media  only screen and (max-width: 500px) {
-.button {
-width: 100% !important;
-}
-}
+    @media only screen and (max-width: 500px) {
+        .button {
+            width: 100% !important;
+        }
+    }
 </style>
-<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; background-color: #404040; margin: 0; padding: 0; width: 100%;"><tr>
-<td align="center" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
-<table class="content" width="100%" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; margin: 0; padding: 0; width: 100%;">
-<tr>
-<td class="header" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; padding: 25px 0; text-align: center;">
-<a href="http://www.nhnevenementen.nl/" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; color: #3d4852; font-size: 19px; font-weight: bold; text-decoration: none; display: inline-block;">
-<img src="http://www.nhnevenementen.nl/img/nz-logo-light.png" class="logo" alt="Laravel Logo" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; max-width: 100%; border: none; height: 75px; width: 75px;"></a>
-</td>
-</tr>
-<!-- Email Body --><tr>
-<td class="body" width="100%" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; background-color: #404040; margin: 0; padding: 0; width: 100%;">
-<table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px; border-color: #e8e5ef; border-radius: 2px; border-width: 1px; box-shadow: 0 2px 0 rgba(0, 0, 150, 0.025), 2px 4px 0 rgba(0, 0, 150, 0.015); margin: 0 auto; padding: 0; width: 570px;">
-<!-- Body content --><tr>
-<td class="content-cell" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; max-width: 100vw; padding: 32px; background-color: #1F1F1F; border-radius: 14px;">
-<h1 style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; color: #C1C1C1; font-size: 18px; font-weight: bold; margin-top: 0; text-align: left;">Hallo!</h1>
-<p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">U heeft zich aangemeld voor het evenement <b>{{ $event->name }} {{ $event->event_id }}</b> dit zal plaats vinden op <b>{{ $event->settings->date_start }}</b>.</p>
-<p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Dit evenement bevind zich in <b>{{$event->settings->location}}</b>.</p>
-<table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; margin: 30px auto; padding: 0; text-align: center; width: 100%;"><tr>
-<td align="center" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
-<table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;"><tr>
-<td align="center" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
-<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;"><tr>
-<td style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
-</td>
-<a href="http://www.nhnevenementen.nl/event/{{ $event->id }}" class="button button-primary" target="_blank" rel="noopener" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif; position: relative; -webkit-text-size-adjust: none; border-radius: 4px; color: #fff; display: inline-block; overflow: hidden; text-decoration: none; background-color: #404040; border-bottom: 8px solid #404040; border-left: 18px solid #404040; border-right: 18px solid #404040; border-top: 8px solid #404040;">Evenement</a>
-</tr></table>
-</td>
-</tr></table>
-</td>
-</tr></table>
-<p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Kan u niet meer komen op het evenement zouden wij dit graag horen</p>
-<p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Met ondernemende groet,<br>
-Nederlandse Zaken</p>
+<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation"
+       style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; background-color: #404040; margin: 0; padding: 0; width: 100%;">
+    <tr>
+        <td align="center" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
+            <table class="content" width="100%" cellpadding="0" cellspacing="0" role="presentation"
+                   style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; margin: 0; padding: 0; width: 100%;">
+                <tr>
+                    <td class="header"
+                        style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; padding: 25px 0; text-align: center;">
+                        <a href="http://www.nhnevenementen.nl/"
+                           style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; color: #3d4852; font-size: 19px; font-weight: bold; text-decoration: none; display: inline-block;">
+                            <img src="http://www.nhnevenementen.nl/img/nz-logo-light.png" class="logo"
+                                 alt="Laravel Logo"
+                                 style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; max-width: 100%; border: none; height: 75px; width: 75px;"></a>
+                    </td>
+                </tr>
+                <!-- Email Body -->
+                <tr>
+                    <td class="body" width="100%" cellpadding="0" cellspacing="0"
+                        style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; background-color: #404040; margin: 0; padding: 0; width: 100%;">
+                        <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0"
+                               role="presentation"
+                               style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px; border-color: #e8e5ef; border-radius: 2px; border-width: 1px; box-shadow: 0 2px 0 rgba(0, 0, 150, 0.025), 2px 4px 0 rgba(0, 0, 150, 0.015); margin: 0 auto; padding: 0; width: 570px;">
+                            <!-- Body content -->
+                            <tr>
+                                <td class="content-cell"
+                                    style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; max-width: 100vw; padding: 32px; background-color: #1F1F1F; border-radius: 14px;">
+                                    <h1 style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; color: #C1C1C1; font-size: 18px; font-weight: bold; margin-top: 0; text-align: left;">
+                                        Hallo!</h1>
+                                    <p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+                                        U heeft zich aangemeld voor het evenement
+                                        <b>{{ $event->name }} {{ $event->event_id }}</b> dit zal plaats vinden op
+                                        <b>{{ $event->settings->date_start }}</b>.</p>
+                                    <p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+                                        Dit evenement bevind zich in <b>{{$event->settings->location}}</b>.</p>
+                                    <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0"
+                                           role="presentation"
+                                           style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; margin: 30px auto; padding: 0; text-align: center; width: 100%;">
+                                        <tr>
+                                            <td align="center"
+                                                style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
+                                                <table width="100%" border="0" cellpadding="0" cellspacing="0"
+                                                       role="presentation"
+                                                       style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
+                                                    <tr>
+                                                        <td align="center"
+                                                            style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
+                                                            <table border="0" cellpadding="0" cellspacing="0"
+                                                                   role="presentation"
+                                                                   style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
+                                                                <tr>
+                                                                    <td style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
+                                                                    </td>
+                                                                    <a href="http://www.nhnevenementen.nl/event/{{ $event->id }}"
+                                                                       class="button button-primary" target="_blank"
+                                                                       rel="noopener"
+                                                                       style="box-sizing: border-box; font-family: 'Quicksand', sans-serif; position: relative; -webkit-text-size-adjust: none; border-radius: 4px; color: #fff; display: inline-block; overflow: hidden; text-decoration: none; background-color: #404040; border-bottom: 8px solid #404040; border-left: 18px solid #404040; border-right: 18px solid #404040; border-top: 8px solid #404040;">Evenement</a>
+                                                                </tr>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+                                        Kan u niet meer komen op het evenement zouden wij dit graag horen</p>
+                                    <p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+                                        Met ondernemende groet,<br>
+                                        Nederlandse Zaken</p>
 
 
-<table class="subcopy" width="100%" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; border-top: 1px solid #e8e5ef; margin-top: 25px; padding-top: 25px;"><tr>
-<td style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
-<p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; line-height: 1.5em; margin-top: 0; text-align: left; font-size: 14px;">
-Werkt de link naar de evenement pagina niet kopieer en plak dan deze link in uw adresbalk: <a href="http://www.nhnevenementen.nl/event/{{ $event->id }}" style="position: relative; color: #3869d4;">http://www.nhnevenementen.nl/event/{{ $event->id }}</a>
-</p>
+                                    <table class="subcopy" width="100%" cellpadding="0" cellspacing="0"
+                                           role="presentation"
+                                           style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; border-top: 1px solid #e8e5ef; margin-top: 25px; padding-top: 25px;">
+                                        <tr>
+                                            <td style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
+                                                <p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; line-height: 1.5em; margin-top: 0; text-align: left; font-size: 14px;">
+                                                    Werkt de link naar de evenement pagina niet kopieer en plak dan deze
+                                                    link in uw adresbalk: <a
+                                                        href="http://www.nhnevenementen.nl/event/{{ $event->id }}"
+                                                        style="position: relative; color: #3869d4;">http://www.nhnevenementen.nl/event/{{ $event->id }}</a>
+                                                </p>
 
-</td>
-</tr></table>
-</td>
-</tr>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
+                        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0"
+                               role="presentation"
+                               style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px; margin: 0 auto; padding: 0; text-align: center; width: 570px;">
+                            <tr>
+                                <td class="content-cell" align="center"
+                                    style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; max-width: 100vw; padding: 32px;">
+                                    <p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; line-height: 1.5em; margin-top: 0; color: #b0adc5; font-size: 12px; text-align: center;">
+                                        © 2020 Nederlandse Zaken.</p>
+
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
 </table>
-</td>
-</tr>
-<tr>
-<td style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative;">
-<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px; margin: 0 auto; padding: 0; text-align: center; width: 570px;"><tr>
-<td class="content-cell" align="center" style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; max-width: 100vw; padding: 32px;">
-<p style="box-sizing: border-box; font-family: 'Quicksand', sans-serif ; position: relative; line-height: 1.5em; margin-top: 0; color: #b0adc5; font-size: 12px; text-align: center;">© 2020 Nederlandse Zaken.</p>
-
-</td>
-</tr></table>
-</td>
-</tr>
-</table>
-</td>
-</tr></table>
 </body>
 </html>
