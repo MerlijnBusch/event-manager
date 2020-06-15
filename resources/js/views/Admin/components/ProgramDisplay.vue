14ab017a9a26b065e0dfd83d0e4457675c4d1798
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-06-15 10:05:18 +0200

    stylefixes program/congress displays admin

--- resources/js/views/Admin/components/ProgramDisplay.vue
+++ resources/js/views/Admin/components/ProgramDisplay.vue
@@ -2,6 +2,7 @@
     <div class="admin-main-program-container">
         <TitleDisplay
             v-if="program"
+            type="program"
             :display="program"
         />
         <div class="admin-item-container">
